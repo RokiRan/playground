7166541f8f00d5216786c4a242d16f3342030028
Roki Ran <ranxiaojing7096@gmail.com>
2022-07-22 15:57:33 +0800

    feat: 鼠标拖动相关

--- src/pages/drag/index.vue
+++ src/pages/drag/index.vue
@@ -1,21 +1,35 @@
 <script lang='ts' setup>
 const target = ref<HTMLImageElement>()
-const x = ref('')
-const y = ref('')
+const box = ref<HTMLImageElement>()
 const offsetTop = ref(0)
-const offsetLfet = ref(0)
+const offsetLeft = ref(0)
 const isStartDrop = ref(false)
+
 const startPoint = reactive({
   x: 0,
   y: 0,
 })
+const mousePoint = reactive({ // 鼠标实时坐标
+  x: 0,
+  y: 0,
+})
+const mouseInScreen = reactive({ x: 0, y: 0 })
+const targetPoint = reactive({ x: 0, y: 0 })
+const boxPosition = reactive({ x: 0, y: 0 })
+const boxContainer = { width: 0, height: 0 } // box 容器大小
 onMounted(() => {
-  x.value = target.value?.style.left || ''
-  y.value = target.value?.style.top || ''
+  // x.value = target.value?.style.left || ''
+  // y.value = target.value?.style.top || ''
   offsetTop.value = target.value?.offsetTop || 0
-  offsetLfet.value = target.value?.offsetLeft || 0
+  offsetLeft.value = target.value?.offsetLeft || 0
+  boxPosition.x = box.value!.offsetTop
+  boxPosition.y = box.value!.offsetLeft
+  boxContainer.height = box.value!.offsetHeight
+  boxContainer.width = box.value!.offsetWidth
 })
 const onMouseDown = (e: MouseEvent) => {
+  if (e.target !== target.value || isStartDrop.value)
+    return
   startPoint.x = e.offsetX
   startPoint.y = e.offsetY
   isStartDrop.value = true // 标记可以拖动
@@ -23,28 +37,44 @@ const onMouseDown = (e: MouseEvent) => {
 const onMouseUp = (e: MouseEvent) => {
   isStartDrop.value = false
 }
-const onDrag = (e: MouseEvent) => {
+const onMove = (e: MouseEvent) => {
+  e.stopPropagation()
+  e.preventDefault()
+  mousePoint.x = e.offsetX
+  mousePoint.y = e.offsetY
+  // console.log(e)
+  mouseInScreen.x = e.clientX // 屏幕坐标
+  mouseInScreen.y = e.clientY
   if (!isStartDrop.value)
     return
-  const x = e.offsetX
-  const y = e.offsetY
-  const offsetPoint = {
-    x: x - startPoint.x,
-    y: y - startPoint.y,
-  }
-  // eslint-disable-next-line no-console
-  console.log(offsetPoint)
-//   target.value!.style.top = `${offsetPoint.x}px`
-//   target.value!.style.left = `${offsetPoint.y}px`
+  let top = (mouseInScreen.y - boxPosition.x) - startPoint.y // 实际坐标 = 鼠标屏幕坐标 - 容器上左偏移 - 目标内鼠标坐标偏移
+  let left = (mouseInScreen.x - boxPosition.y) - startPoint.x
+  // 判断边界
+  if (top < 0)
+    top = 0
+  if (top > boxContainer.height - target.value!.offsetHeight)
+    top = boxContainer.height - target.value!.offsetHeight - 2
+  if (left < 0)
+    left = 0
+  if (left > boxContainer.width - target.value!.offsetWidth)
+    left = boxContainer.width - target.value!.offsetWidth - 2
+  target.value!.style.top = `${top}px`
+  target.value!.style.left = `${left}px`
 }
 </script>
 
 <template>
-  <div w-100 h-100 border-1 mx-auto pos="relative">
-    <div ref="target" w-20 h-20 border-1 inline-block pos="absolute" cursor="hover:move" @mousemove="onDrag" @mousedown="onMouseDown" @mouseup="onMouseUp" />
+  <div ref="box" w-100 h-100 border-1 mx-auto pos="relative" @mousemove="onMove" @mouseup="onMouseUp">
+    <div ref="target" w-20 h-20 border-1 inline-block top-20 left-20 pos="absolute" cursor="hover:move" @mousedown="onMouseDown" />
   </div>
   <div>
-    <span>offsetTop:{{ offsetTop }}</span> <br>
-    <span>offsetLfet:{{ offsetLfet }}</span>
+    <span>boxPosition: {{ boxPosition.x }} , {{ boxPosition.y }}</span> <br>
+    <span>startPoint: {{ startPoint.x }} , {{ startPoint.y }}</span> <br>
+    <span>offsetTop: {{ offsetTop }}</span> <br>
+    <span>offsetLeft: {{ offsetLeft }}</span><br>
+    <!-- <span>movementX: {{ mx }}</span><br> -->
+    <span>mousePoint: {{ mousePoint.x }} , {{ mousePoint.y }}</span><br>
+    <span>targetPoint: {{ targetPoint.x }} , {{ targetPoint.y }}</span><br>
+    <span>mouseInScreen: {{ mouseInScreen.x }} , {{ mouseInScreen.y }}</span><br>
   </div>
 </template>
---
+++ src/pages/drag/tip.md
@@ -0,0 +1,2 @@
+1. event.clientX 屏幕坐标
+2. move事件中，offsetX 和 Y 只针对鼠标所在的对象
