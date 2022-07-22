fc6e9542db975829dcfffcfe0badaaf742c55cc8
Roki Ran <ranxiaojing7096@gmail.com>
2022-07-22 16:35:00 +0800

    feat: slider 未完成

--- src/pages/slider/index.vue
+++ src/pages/slider/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 const imgs = ['https://cdn.pixabay.com/photo/2015/01/07/15/51/woman-591576_960_720.jpg', 'https://cdn.pixabay.com/photo/2014/07/31/22/50/photographer-407068_960_720.jpg', 'https://cdn.pixabay.com/photo/2020/02/02/14/03/cat-4813099_960_720.jpg']
-const slider = ref<HtmlDivElement>()
-const offsetX = 0
+const slider = ref<HTMLDivElement>()
+const isStartDrop = ref(false)
+const startPoint = reactive({ x: 0, y: 0 })
 onMounted(() => {
-  console.log(slider)
   // window.setInterval(() => {
   //   slider.value.scrollBy(offsetX++, 0)
   //   console.log(slider.value.scrollWidth, offsetX)
@@ -13,36 +13,49 @@ onMounted(() => {
 })
 const moveForward = () => {
   // slider.value.offsetX = 100
-  slider.value.scrollBy(400, 0)
+  slider.value!.scrollBy(400, 0)
 }
 const moveBack = () => {
   // slider.value.offsetX = 100
-  slider.value.scrollBy(-400, 0)
+  slider.value!.scrollBy(-400, 0)
 }
-const onDrag = (e: DragEvent) => {
+const onDrag = (e: MouseEvent) => {
   // slider.value
-  const deltaLeft = e.clientX - e.target.offsetLeft
-  const deltaTop = e.clientY - e.target.offsetTop
-  console.log(deltaLeft, deltaTop)
+  // const deltaLeft = e.clientX - e.target!.offsetLeft
+  // const deltaTop = e.clientY - e.target!.offsetTop
+  if (!isStartDrop.value)
+    return
+  slider.value!.scrollBy(e.offsetX - startPoint.x, 0)
+}
+const onMouseDown = (e: MouseEvent) => {
+  isStartDrop.value = true
+  startPoint.x = e.offsetX
+}
+const onMouseUp = (e: MouseEvent) => {
+  isStartDrop.value = false
+  isStartDrop.value = false
+  startPoint.x = 0
 }
 </script>
 
 <template>
   <div>
-    <div ref="slider" flex="~ row" w-100 overflow="hidden" mx-auto transform @drag="onDrag">
+    <div pos="relative" w-100 mx-auto>
       <div
         i-carbon-chevron-left
-        inline-block pos="fixed top-31"
+        inline-block pos="absolute top-31 left-0"
         cursor="pointer"
         @click="moveBack"
       />
       <div
         i-carbon-chevron-right
-        inline-block pos="fixed top-31 right-1"
+        inline-block pos="absolute top-31 right-1"
         cursor="pointer"
         @click="moveForward"
       />
-      <img v-for="img in imgs" :key="img" :src="img" w-100>
+      <div ref="slider" flex="~ row" w-100 overflow="hidden" mx-auto @mousemove="onDrag" @mousedown="onMouseDown" @mouseup="onMouseUp">
+        <img v-for="img in imgs" :key="img" :src="img" w-100>
+      </div>
     </div>
   </div>
 </template>
