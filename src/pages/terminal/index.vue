1aa02dbaaa78c24fb28ff14d193abefe85600f15
Roki Ran <ranxiaojing7096@gmail.com>
2022-08-12 10:26:32 +0800

    feat: 微信自动播放音频-失败

--- src/pages/terminal/index.vue
+++ src/pages/terminal/index.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
 const Audio = ref<HTMLAudioElement>()
 const Video = ref<HTMLVideoElement>()
+const AudioBtn = ref<HTMLDivElement>()
+const VideoBtn = ref<HTMLDivElement>()
 onMounted(() => {
-  nextTick(() => {
-    // eslint-disable-next-line no-console
-    console.log('开始播放')
-    Video.value!.play()
-    Audio.value!.play()
-  })
+  Audio.value!.play()
+  Video.value!.play()
 })
+document.addEventListener('WeixinJSBridgeReady', () => {
+  // eslint-disable-next-line no-console
+  console.log('WeixinJSBridgeReady')
+  Audio.value!.play()
+  Video.value!.play()
+}, false)
 </script>
 
 <template>
   <div mt-5>
-    <audio ref="Audio" mx-auto src="/audio/孙伯纶 - 如果你也听说 (选择导师：那英).mp3" w-90vw h-12 controls />
+    <audio ref="Audio" mx-auto src="/audio/孙伯纶 - 如果你也听说 (选择导师：那英).mp3" w-90vw h-12 controls autoplay />
   </div>
   <div mt-5>
     <video ref="Video" mx-auto src="/video/yourName.mp4" w-90vw controls autoplay />
+  </div>
+  <div>
+    <div ref="AudioBtn" class="btn" @click="Video?.play()">
+      播放视频
+    </div>
+    <div ref="VideoBtn" ml-5 mt-5 class="btn" @click="Audio?.play()">
+      播放音频
+    </div>
   </div>
 </template>
 
